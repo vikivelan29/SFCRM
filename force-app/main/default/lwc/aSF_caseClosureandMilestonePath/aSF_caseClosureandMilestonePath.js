a8e6ee0eee2f1ac4f88e2bc725b44d2fabcd0174
Santanu Halder <38907154+santanuhaldersfdc@users.noreply.github.com>
2023-12-07 21:49:08 +0530

    Initial commit

--- force-app/main/default/lwc/aSF_caseClosureandMilestonePath/aSF_caseClosureandMilestonePath.js
+++ force-app/main/default/lwc/aSF_caseClosureandMilestonePath/aSF_caseClosureandMilestonePath.js
@@ -6,9 +6,12 @@ import CASE_STAGE from '@salesforce/schema/Case.Stage__c';
 import CASE_CLOSE_SLA from '@salesforce/schema/Case.Overall_Case_Closure_SLA__c';
 import CASE_STAGE_SLA_1 from '@salesforce/schema/Case.Stage_SLA_1__c';
 import fetchCase from '@salesforce/schema/Case.CaseNumber';
+import { registerListener, unregisterAllListeners } from 'c/asf_pubsub';
+import { CurrentPageReference, NavigationMixin } from 'lightning/navigation';
+import { refreshApex } from '@salesforce/apex';
 const fields = [CASE_STAGE,CASE_CLOSE_SLA,CASE_STAGE_SLA_1,fetchCase];
 
-export default class ASF_caseClosureandMilestonePath extends LightningElement {
+export default class ASF_caseClosureandMilestonePath extends NavigationMixin(LightningElement) {
     @api recordId;
     timer;
     wiredData;
@@ -167,6 +170,18 @@ export default class ASF_caseClosureandMilestonePath extends LightningElement {
             }
             
         }, 1000);
+    }
+    connectedCallback(){
+        registerListener("refreshpagepubsub", this.handlePublishedMessage, this);
+    }
+    @wire(CurrentPageReference) pageRef;
+
+    handlePublishedMessage(payload) {
+        console.log('handlePublishedMessage of case SLA');
+        if (this.recordId == recordId) {
+            refreshApex(this.caseObj);
+        }
+
     }
     disconnectedCallback(){
         clearInterval(this.timerId1);
--- force-app/main/default/lwc/asf_ReopenCase/asf_ReopenCase.js
+++ force-app/main/default/lwc/asf_ReopenCase/asf_ReopenCase.js
@@ -16,6 +16,7 @@ import ID_FIELD from "@salesforce/schema/Case.Id";
 import { CloseActionScreenEvent } from 'lightning/actions';
 import { NavigationMixin } from "lightning/navigation";
 import { reduceErrors } from 'c/asf_ldsUtils';
+import { fireEventNoPageRef, registerListener } from 'c/asf_pubsub';
 
 export default class Asf_ReopenCase extends NavigationMixin(LightningElement) {
 
@@ -49,14 +50,17 @@ export default class Asf_ReopenCase extends NavigationMixin(LightningElement) {
                 title: 'Case Reopened Successfully',
                 message: 'Case is successfully reopened!'
             });
-            this[NavigationMixin.Navigate]({
-                type: "standard__recordPage",
-                attributes: {
-                    objectApiName: "Case",
-                    actionName: "view",
-                    recordId: this.recordId
-                }
-            });
+            // this[NavigationMixin.Navigate]({
+            //     type: "standard__recordPage",
+            //     attributes: {
+            //         objectApiName: "Case",
+            //         actionName: "view",
+            //         recordId: this.recordId
+            //     }
+            // });
+            console.log('Firing pubsub from Reopen!!!!!!');
+            let payload = {'source':'reopencase', 'recordId':this.recordId};
+            fireEventNoPageRef(this.pageRef, "refreshpagepubsub", payload);
             this.closeAction();
 
         })
