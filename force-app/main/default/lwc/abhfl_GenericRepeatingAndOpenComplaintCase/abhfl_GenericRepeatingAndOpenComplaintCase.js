1200e692bc603cb39029ffdab6b783f9bc36f753
AnirudhRaturi <anirudh.raturi@salesforce.com>
2024-03-15 18:12:44 +0530

    Change

--- force-app/main/default/lwc/abhfl_GenericRepeatingAndOpenComplaintCase/abhfl_GenericRepeatingAndOpenComplaintCase.js
+++ force-app/main/default/lwc/abhfl_GenericRepeatingAndOpenComplaintCase/abhfl_GenericRepeatingAndOpenComplaintCase.js
@@ -22,7 +22,7 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Lightnin
     };
 
     renderedCallback() {
-        if(this.objectApiName == 'Account') {
+        if (this.objectApiName == 'Account') {
             this.addIconClass("[data-id='Repeated_Indicator_div']", 'slds-hide');
         }
     }
@@ -31,24 +31,27 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Lightnin
         this.getCaseRecord();
     }
 
-   async getCaseRecord() {
+    async getCaseRecord() {
 
         this.isLoaded = true;
-        if(this.objectApiName == 'Case') {
+        if (this.objectApiName == 'Case') {
 
-            let fieldArray    = "id, AccountId, LAN__c, Nature__c, Type_Text__c, Sub_Type_Text__c";
+            let fieldArray = "id, AccountId, LAN__c, Nature__c, Type_Text__c, Sub_Type_Text__c";
             let whereClauseOfRI = "WHERE id ='" + this.recordId + "' WITH SECURITY_ENFORCED";
 
-            let rec = await getRecords({ fields: fieldArray, objectName: this.objectApiName, whereClause: whereClauseOfRI});
-            if(rec) {
+            let rec = await getRecords({ fields: fieldArray, objectName: this.objectApiName, whereClause: whereClauseOfRI });
+            if (rec) {
                 this.caseRecord = rec[0];
-                if(!this.whereClauseForRI && !this.whereClauseForOC) {
-                    this.initializeWhereClause();
+                let caseAccId = this.caseRecord.hasOwnProperty('AccountId') ? this.caseRecord.AccountId : '';
+                if(caseAccId) {             
+                    if (!this.whereClauseForRI && !this.whereClauseForOC && caseAccId) {
+                        this.initializeWhereClause();
+                    } 
+                    this.story_328_329_330();              
                 }
-                this.story_328_329_330();
             }
         }
-        else if(this.objectApiName == 'Account') {
+        else if (this.objectApiName == 'Account') {
             this.addIconClass("[data-id='Repeated_Indicator']", 'slds-hide');
             this.removeIconClass("[data-id='Open_Complaint_Indicator_div']", 'slds-hide');
             this.initializeWhereClause();
@@ -61,7 +64,7 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Lightnin
     story_328_329_330() {
 
         let caseNature = this.caseRecord.Nature__c;
-        if(caseNature == "Complaint") {
+        if (caseNature == "Complaint") {
             this.story_328_329();
         }
         else {
@@ -75,23 +78,23 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Lightnin
         let withSecEnforced = 'WITH SECURITY_ENFORCED';
         let commonForOC = `WHERE Stage__c = \'Open\' AND Nature__c = \'Complaint\'`;
 
-        if(this.objectApiName == 'Case') {
+        if (this.objectApiName == 'Case') {
             this.whereClauseForRI = `WHERE id != \'${this.recordId}\' AND AccountId = \'${this.caseRecord.AccountId}\' AND Stage__c = \'Resolved\' AND LAN__c = \'${this.caseRecord.LAN__c}\'
                                 AND Nature__c = \'${this.caseRecord.Nature__c}\' AND Type_Text__c = \'${this.caseRecord.Type_Text__c}\' 
                                 AND Sub_Type_Text__c = \'${this.caseRecord.Sub_Type_Text__c}\' AND CreatedDate = LAST_N_DAYS:${this.customLabel.Days_For_Repeated_Indicator} 
-                                 ${withSecEnforced} LIMIT 2`; 
-             
-            this.whereClauseForOC = `${commonForOC} AND AccountId = \'${this.caseRecord.AccountId}\'  ${withSecEnforced} LIMIT 1`;                             
+                                 ${withSecEnforced} LIMIT 2`;
+
+            this.whereClauseForOC = `${commonForOC} AND AccountId = \'${this.caseRecord.AccountId}\'  ${withSecEnforced} LIMIT 1`;
         }
-        else if(this.objectApiName == 'Account') {
+        else if (this.objectApiName == 'Account') {
             this.whereClauseForOC = `${commonForOC} AND AccountId = \'${this.recordId}\'  ${withSecEnforced} LIMIT 1`;
         }
     }
-    
+
     async story_328_329() {
         let iconAttrObjOC = {};
-        let sobRecords = await getRecords({ fields: this.fieldArr, objectName: this.objectApiName, whereClause: this.whereClauseForOC});
-        if(sobRecords && sobRecords.length > 0) {
+        let sobRecords = await getRecords({ fields: this.fieldArr, objectName: this.objectApiName, whereClause: this.whereClauseForOC });
+        if (sobRecords && sobRecords.length > 0) {
             iconAttrObjOC.dataId = '[data-id="Open_Complaint_Indicator"]';
             iconAttrObjOC.variant = 'error';
             this.addAndChangeAttributes(iconAttrObjOC);
@@ -102,8 +105,8 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Lightnin
 
     async story_330() {
         let iconAttrObjRI = {};
-        let sobRecords = await getRecords({ fields: this.fieldArr, objectName: this.objectApiName, whereClause: this.whereClauseForRI});
-        if(sobRecords && sobRecords.length > 0) {
+        let sobRecords = await getRecords({ fields: this.fieldArr, objectName: this.objectApiName, whereClause: this.whereClauseForRI });
+        if (sobRecords && sobRecords.length > 0) {
             iconAttrObjRI.dataId = '[data-id="Repeated_Indicator"]';
             iconAttrObjRI.variant = 'error';
             this.addAndChangeAttributes(iconAttrObjRI);
@@ -118,14 +121,14 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Lightnin
 
     addIconClass(dataId, iconClass) {
         let getLightningIcon = this.template.querySelector(dataId);
-        if(getLightningIcon) {
+        if (getLightningIcon) {
             getLightningIcon.classList.add(iconClass);
         }
     }
 
     removeIconClass(dataId, iconClass) {
         let getLightningIcon = this.template.querySelector(dataId);
-        if(getLightningIcon) {
+        if (getLightningIcon) {
             getLightningIcon.classList.remove(iconClass);
         }
     }
