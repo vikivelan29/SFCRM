aa4c33be8300c5c1c417fb901e76368fd7184637
Santosh Vyavhare <savyavhare-ext@airfrance.fr>
2024-03-21 16:52:28 +0530

    575- case source update

--- force-app/main/default/lwc/asf_Utility/asf_Utility.js
+++ force-app/main/default/lwc/asf_Utility/asf_Utility.js
@@ -100,7 +100,7 @@ export class asf_Utility {
 
         fields[ID_FIELD.fieldApiName] = parentJS.recordId;
         fields[NEW_STAGE.fieldApiName] = true;
-
+        
        if(!parentJS.rejectCase){
         if(source != 'Email'){
            fields[SUBJECT_FIELD.fieldApiName] = 'SR : '+selected.Type__c;
@@ -112,7 +112,7 @@ export class asf_Utility {
        
        fields[NATURE_FIELD.fieldApiName] = parentJS.natureVal;
        //fields[PRODUCT_FIELD.fieldApiName] = parentJS.productVal;
-       fields[SOURCE_FIELD.fieldApiName] = source;
+       //fields[SOURCE_FIELD.fieldApiName] = source;
        //fields[CASE_ORIGIN.fieldApiName] = parentJS.originValue;
        fields[TECHNICAL_SOURCE_FIELD.fieldApiName] = 'LWC';
        fields[CASE_BUSINESSUNIT.fieldApiName] = parentJS.businessUnitValue; 
