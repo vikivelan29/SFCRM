5ea5520a84e91fdb1e6f5c3b74bd0989230fb4fe
sripavi4 <sripavithra4@gmail.com>
2024-04-04 17:41:54 +0530

    bug fix

--- force-app/main/default/aura/ASF_deleteContentURL/ASF_deleteContentURLController.js
+++ force-app/main/default/aura/ASF_deleteContentURL/ASF_deleteContentURLController.js
@@ -7,32 +7,37 @@
         console.log('inside do init-'+JSON.stringify(component.get("v.pageReference")));  
     },
     deleteFile: function(component, event, helper) {
-        var recordId = component.get("v.recordId");
-        var action = component.get("c.deleteFileById");
-        action.setParams({ fileId: recordId });
-        action.setCallback(this, function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                console.log("File deleted successfully");
-                if(response.getReturnValue() != 'Error'){
-                    component.set("v.contentDocId",response.getReturnValue());
+        var comments = component.find("comment");
+        if(!comments.checkValidity()) {
+            comments.showHelpMessageIfInvalid();
+        }else{
+            var recordId = component.get("v.recordId");
+            var action = component.get("c.deleteFileById");
+            action.setParams({ fileId: recordId, comment: component.get("v.comments") });
+            action.setCallback(this, function(response) {
+                var state = response.getState();
+                if (state === "SUCCESS") {
+                    console.log("File deleted successfully");
+                    if(response.getReturnValue() != 'Error'){
+                        component.set("v.contentDocId",response.getReturnValue());
+                    }
+                    helper.closeTab(component,'delete');
+                    
+                } else if (state === "ERROR") {
+                    // Display error message
+                    var errors = response.getError();
+                    if (errors && errors[0] && errors[0].message) {
+                        var errorMessage = errors[0].message;
+                        var validationMessage = '';
+                        var startIndex = errorMessage.indexOf("FIELD_CUSTOM_VALIDATION_EXCEPTION");
+                        errorMessage = errorMessage.split("FIELD_CUSTOM_VALIDATION_EXCEPTION,")[1];
+                        errorMessage = errorMessage.split(":")[0];
+                        component.set("v.errorMessage", errorMessage);
+                    }
                 }
-                helper.closeTab(component,'delete');
-                
-            } else if (state === "ERROR") {
-                // Display error message
-                var errors = response.getError();
-                if (errors && errors[0] && errors[0].message) {
-                    var errorMessage = errors[0].message;
-                    var validationMessage = '';
-                    var startIndex = errorMessage.indexOf("FIELD_CUSTOM_VALIDATION_EXCEPTION");
-                    errorMessage = errorMessage.split("FIELD_CUSTOM_VALIDATION_EXCEPTION,")[1];
-                    errorMessage = errorMessage.split(":")[0];
-                    component.set("v.errorMessage", errorMessage);
-                }
-            }
-        });
-        $A.enqueueAction(action);
+            });
+            $A.enqueueAction(action);
+        }
     },
     goback : function(component, event, helper) {
         helper.fetchContentDocId(component, event, 'back');
