cf020a1d588f6f657137242d3a43faf1a29d2e37
Santosh Vyavhare <savyavhare-ext@airfrance.fr>
2024-06-04 15:16:19 +0530

    cannot cho0se duplicate as parent
    cannot cho0se duplicate as parent

--- force-app/main/default/lwc/asf_RelateDuplicateCase/asf_RelateDuplicateCase.js
+++ force-app/main/default/lwc/asf_RelateDuplicateCase/asf_RelateDuplicateCase.js
@@ -14,6 +14,7 @@ import TYPE_FIELD from '@salesforce/schema/Case.Sub_Type_Text__c';
 import SUBTYPE_FIELD from '@salesforce/schema/Case.Type_Text__c';
 import OWNER_FIELD from '@salesforce/schema/Case.OwnerId';
 import BUSINESS_UNIT_FIELD from '@salesforce/schema/Case.Business_Unit__c'
+import IS_DUPLICATE_FIELD from '@salesforce/schema/Case.Is_Duplicate__c'
 import USER_ID from '@salesforce/user/Id';
 
 export default class Asf_RelateDeduplicateCase extends LightningElement {
@@ -27,7 +28,7 @@ export default class Asf_RelateDeduplicateCase extends LightningElement {
     wiredCurrentRec;
     userId = USER_ID;
     errorMessage = 'You do not have access. Only case owner is allowed to mark the case as Relate/ Duplicate';
-    @api caseFields = [LAN_FIELD, ISCLOSED_FIELD, PARENTCASEID_FIELD, CATEGORY_FIELD, TYPE_FIELD, SUBTYPE_FIELD, OWNER_FIELD, BUSINESS_UNIT_FIELD];
+    @api caseFields = [LAN_FIELD, ISCLOSED_FIELD, PARENTCASEID_FIELD, CATEGORY_FIELD, TYPE_FIELD, SUBTYPE_FIELD, OWNER_FIELD, BUSINESS_UNIT_FIELD, IS_DUPLICATE_FIELD];
     
     @wire(getRecord, { recordId: '$recordId', fields: '$caseFields' })
     wiredRecord({ error, data }) {
@@ -39,7 +40,8 @@ export default class Asf_RelateDeduplicateCase extends LightningElement {
                     Type: getFieldValue(data, TYPE_FIELD),
                     SubType: getFieldValue(data, SUBTYPE_FIELD),
                     Owner: getFieldValue(data, OWNER_FIELD),
-                    BusinessUnit : getFieldValue(data, BUSINESS_UNIT_FIELD)
+                    BusinessUnit : getFieldValue(data, BUSINESS_UNIT_FIELD),
+                    IsDuplicate : getFieldValue(data, IS_DUPLICATE_FIELD)
             };
             this.parentCaseId = getFieldValue(data, PARENTCASEID_FIELD);
             this.ownerValidation();
@@ -56,7 +58,8 @@ export default class Asf_RelateDeduplicateCase extends LightningElement {
                 Category: getFieldValue(data, CATEGORY_FIELD),
                 Type: getFieldValue(data, TYPE_FIELD),
                 SubType: getFieldValue(data, SUBTYPE_FIELD),
-                BusinessUnit : getFieldValue(data, BUSINESS_UNIT_FIELD)
+                BusinessUnit : getFieldValue(data, BUSINESS_UNIT_FIELD),
+                IsDuplicate : getFieldValue(data, IS_DUPLICATE_FIELD)
         };
         } else if (error) {
             console.error('Error loading parent record', error);
@@ -94,7 +97,11 @@ export default class Asf_RelateDeduplicateCase extends LightningElement {
 
     validateDuplicateCase(){
         let isValid = true;
-        if(this.wiredParentRec.IsClosed && this.wiredParentRec.BusinessUnit !='ABFL' && this.wiredParentRec.BusinessUnit!='ABWM' && this.wiredCurrentRec.BusinessUnit !='ABFL' && this.wiredCurrentRec.BusinessUnit!='ABWM'){
+        if(this.wiredParentRec.IsDuplicate && (this.wiredParentRec.BusinessUnit =='ABFL' || this.wiredParentRec.BusinessUnit=='ABWM') && (this.wiredCurrentRec.BusinessUnit =='ABFL' || this.wiredCurrentRec.BusinessUnit=='ABWM')){
+            isValid = false;
+            this.showToastMessage('Error!', 'You cannot choose a duplicate case as parent', 'error');
+        }
+        else if(this.wiredParentRec.IsClosed && this.wiredParentRec.BusinessUnit !='ABFL' && this.wiredParentRec.BusinessUnit!='ABWM' && this.wiredCurrentRec.BusinessUnit !='ABFL' && this.wiredCurrentRec.BusinessUnit!='ABWM'){
             isValid = false;
             this.showToastMessage('Error!', 'You cannot choose a closed case as parent', 'error');
         }
@@ -108,6 +115,7 @@ export default class Asf_RelateDeduplicateCase extends LightningElement {
             isValid = false;
             this.showToastMessage('Error!', 'Parent case should belong to same Category, Type and Sub Type as current case', 'error');
         }
+        return ;
         return isValid;
     }
 
