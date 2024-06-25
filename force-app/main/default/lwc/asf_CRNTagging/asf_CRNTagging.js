5d0d6bc593e3111375de5b365411fc2217dfd102
sripavi4 <sripavithra4@gmail.com>
2024-06-25 14:30:44 +0530

    revert

--- force-app/main/default/lwc/asf_CRNTagging/asf_CRNTagging.js
+++ force-app/main/default/lwc/asf_CRNTagging/asf_CRNTagging.js
@@ -36,7 +36,7 @@ export default class Asf_CRNTagging extends LightningElement {
     initialRecords;
     inpValueA;
     @track preSelectedRows = undefined;
-    @track preSelectedAsset = undefined;
+    preSelectedAsset = [];
     prestdAcctId;
     noUpdate = noUpdate;
     @track showLANForCustomer = false;
