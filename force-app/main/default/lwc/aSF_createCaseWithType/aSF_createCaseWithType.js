b21a21bbd5f60f124cf7ec9b6347f7b5c80d0ede
sripavi4 <sripavithra4@gmail.com>
2024-07-03 21:43:12 +0530

    label changes

--- force-app/main/default/lwc/aSF_createCaseWithType/aSF_createCaseWithType.js
+++ force-app/main/default/lwc/aSF_createCaseWithType/aSF_createCaseWithType.js
@@ -19,6 +19,7 @@ import ACOUNNTRECORDTYPE from '@salesforce/schema/Case.Account.RecordType.Name';
 import NOAUTOCOMM_FIELD from '@salesforce/schema/Case.No_Auto_Communication__c';
 import ABSLI_BU from '@salesforce/label/c.ABSLI_BU'; 
 import ABSLIG_BU from '@salesforce/label/c.ABSLIG_BU';
+import { lanLabels } from 'c/asf_ConstantUtility';
 
 //tst strt
 import NATURE_FIELD from '@salesforce/schema/Case.Nature__c';
@@ -116,7 +117,7 @@ export default class ASF_createCaseWithType extends NavigationMixin(LightningEle
     caseRecord;
     showSRModal = false;
     showFAmsg = true;
-    faValidMsg = FAmsg;
+    faValidMsg;
     isFTRJourney = false;
     showSRDescription = false;
     caseDescriptionFTR;
@@ -126,7 +127,7 @@ export default class ASF_createCaseWithType extends NavigationMixin(LightningEle
     complaintValues = [];
     complaintSelected;
     subsourceSelected;
-    withFALabel = WithFA;
+    withFALabel;
     withoutFALabel = WithoutFA;
     selectedSRCategory
 
@@ -328,21 +329,10 @@ export default class ASF_createCaseWithType extends NavigationMixin(LightningEle
     user({ error, data}) {
         if (data){
            this.businessUnit = getFieldValue(data, BUSINESS_UNIT);
-           if(this.businessUnit === 'ABHFL' || this.businessUnit === ABSLI_BU || this.businessUnit === ABSLIG_BU){
-            this.cols = [
-                { label: 'Nature', fieldName: 'Nature__c', type: 'text' },
-                { label: 'Type', fieldName: 'Type__c', type: 'text' },
-                { label: 'Sub Type', fieldName: 'Sub_Type__c', type: 'text' }
-            ];
-            }else{
-            this.cols = [
-                { label: 'Nature', fieldName: 'Nature__c', type: 'text' },
-                { label: 'LOB', fieldName: 'LOB__c', type: 'text' },
-                { label: 'Type', fieldName: 'Type__c', type: 'text' },
-                { label: 'Sub Type', fieldName: 'Sub_Type__c', type: 'text' }
-            ];
-            }
-        }else if (error){
+           this.cols = lanLabels[this.businessUnit].CTST_COLS != null? lanLabels[this.businessUnit].CTST_COLS : lanLabels["DEFAULT"].CTST_COLS;
+           this.faValidMsg = lanLabels[this.businessUnit].FA_VALIDATION_MESSAGE != null? lanLabels[this.businessUnit].FA_VALIDATION_MESSAGE : lanLabels["DEFAULT"].FA_VALIDATION_MESSAGE;
+           this.withFALabel = lanLabels[this.businessUnit].CREATE_CASE_WITH_FA != null? lanLabels[this.businessUnit].CREATE_CASE_WITH_FA : lanLabels["DEFAULT"].CREATE_CASE_WITH_FA;
+        } else if (error){
             console.log('error in get picklist--'+JSON.stringify(error));
         }
     }
@@ -891,7 +881,7 @@ export default class ASF_createCaseWithType extends NavigationMixin(LightningEle
             this.dispatchEvent(
                 new ShowToastEvent({
                     title: 'Error',
-                    message: FA_Mandatory,
+                    message: lanLabels[this.businessUnit].FA_MANDATORY_PREFRAMEWORK != null? lanLabels[this.businessUnit].FA_MANDATORY_PREFRAMEWORK : lanLabels["DEFAULT"].FA_MANDATORY_PREFRAMEWORK,
                     variant: 'Error',
                 }),
             );
