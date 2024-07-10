6cf8e125612b349bb424fd260d5dc052f86735cb
adi-meh <153297645+adi-meh@users.noreply.github.com>
2024-07-10 17:58:37 +0530

    Delta Updates

--- force-app/main/default/lwc/asf_CreateCaseValidations/asf_CreateCaseValidations.js
+++ force-app/main/default/lwc/asf_CreateCaseValidations/asf_CreateCaseValidations.js
@@ -1,31 +1,11 @@
 import { LightningElement } from 'lwc';
-import genFundApiValidationCallout from '@salesforce/apex/ABSLI_CreateCaseValidationsController.genFundApiValidationCallout';
 import nomineeChangeCaseCreationValidation from '@salesforce/apex/ABSLI_CreateCaseValidationsController.nomineeChangeCaseCreationValidation';
-import duplicatePolicyPrintingValidation from '@salesforce/apex/ABSLI_CreateCaseValidationsController.duplicatePolicyPrintingValidation';
-import performUINapiCalloutValidation from '@salesforce/apex/ABSLI_CreateCaseValidationsController.performUINapiCallout';
+
 /**
- * Gen Fund validation method with Apex invocation
+ *
  * @param {*} input 
  * @returns instance of ValidationWrapper
  */
-const genFundApiValidation = async (input) => {
-    try{
-        let result = await genFundApiValidationCallout({caseRecord:JSON.stringify(input.fields)});
-        if(result){
-            //if result is as expected, then
-            if(result=='Success'){
-                return new ValidationWrapper(true, result);
-            }else{
-                return new ValidationWrapper(false, result);
-            }
-           //success response
-        }
-    } catch(error){
-        console.log('genFundApiValidation'+JSON.stringify(error));
-        return new ValidationWrapper(false, error.message.body);//error response
-    }
-}
-
 const nomineeChangeValidation = async (input) => {
     try{
         let result = await nomineeChangeCaseCreationValidation({caseRecord:JSON.stringify(input.fields)});
@@ -44,46 +24,10 @@ const nomineeChangeValidation = async (input) => {
     }
 }
 
-const duplicatePolicyPrinting = async (input) => {
-    try{
-        let result = await duplicatePolicyPrintingValidation({caseRecord:JSON.stringify(input.fields)});
-        if(result){
-            //if result is as expected, then
-            if(result=='Success'){
-                return new ValidationWrapper(true, result);
-            }else{
-                return new ValidationWrapper(false, result);
-            }
-           //success response
-        }
-    } catch(error){
-        console.log('duplicatePolicyPrintingValidation'+JSON.stringify(error));
-        return new ValidationWrapper(false, error.message.body);//error response
-    }
-}
-
-const performUINapiCallout = async (input) => {
-    try{
-        let result = await performUINapiCalloutValidation({caseRecord:JSON.stringify(input.fields)});
-        if(result){
-            //if result is as expected, then
-            if(result=='Success'){
-                return new ValidationWrapper(true, result);
-            }else{
-                return new ValidationWrapper(false, result);
-            }
-           //success response
-        }
-    } catch(error){
-        console.log('performUINapiCalloutValidation'+JSON.stringify(error));
-        return new ValidationWrapper(false, error.message.body);//error response
-    }
-}
-
 
 //include new validation methods inside method export block
 export {
-    genFundApiValidation,nomineeChangeValidation
+    nomineeChangeValidation
 }
 
 //---------------FRAMEWORK CODE - DO NOT TOUCH--------------//
