d7026fb494e3cc72944753868c6140cc8f9080f7
dhineshkumar.teekara <dhineshkumar.teekara@petronas.com>
2024-07-31 13:10:31 +0530

    MultiPicklist Max Value Restriction

--- force-app/main/default/lwc/asf_searchablePicklist/asf_searchablePicklist.js
+++ force-app/main/default/lwc/asf_searchablePicklist/asf_searchablePicklist.js
@@ -22,6 +22,8 @@ export default class Asf_searchablePicklist extends LightningElement {
     selectedValues = [];
     @track mapReturnVal = new Map();
     @track dependentPicklist = false;
+    @api maxMultiSelectAllowed;
+    @track bmaxMultipleSelected = false;
 
 
     connectedCallback() {
@@ -248,12 +250,18 @@ export default class Asf_searchablePicklist extends LightningElement {
             this.serverSidePiclistVal.forEach(entry => {
                 if (entry.value.toLowerCase().trim() == optionVal.trim().toLowerCase()) {
                     if (this.multiselect == "true" || this.multiselect == true) {
-                        if (this.selectedValues.includes(entry.value)) {
-                            this.selectedValues.splice(this.selectedValues.indexOf(entry.value), 1);
+                        if(this.selectedValues.length >= this.maxMultiSelectAllowed){
+                            // do nothing.
                         }
-                        else {
-                            this.selectedValues.push(entry.value);
+                        else{
+                            if (this.selectedValues.includes(entry.value)) {
+                                this.selectedValues.splice(this.selectedValues.indexOf(entry.value), 1);
+                            }
+                            else {
+                                this.selectedValues.push(entry.value);
+                            }
                         }
+                        
                     }
                     else {
                         this.selectedValue = optionVal;
@@ -354,7 +362,7 @@ export default class Asf_searchablePicklist extends LightningElement {
         }
     }
     get showHideRequired() {
-        return this.required;
+        return this.required && !this.bDisabled;
     }
 
     @api validateCustomPicklistField() {
