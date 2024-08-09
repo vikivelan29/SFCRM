9530e76757ca0bcac00f609bf210a2e6a52867c9
dhineshkumar.teekara <dhineshkumar.teekara@petronas.com>
2024-08-09 12:32:36 +0530

    FetchBankDetail Response error fix

--- force-app/main/default/lwc/absli_FetchBankDetails/absli_FetchBankDetails.js
+++ force-app/main/default/lwc/absli_FetchBankDetails/absli_FetchBankDetails.js
@@ -74,7 +74,10 @@ export default class Absli_FetchBankDetails extends LightningElement {
             if (!this.processApexReturnValue || Object.keys(this.processApexReturnValue).length === 0) {
                 this.showToast('Unable to fetch Bank Details, please try again later.', 'error'); 
                 return;
-            }
+            } else if (this.processApexReturnValue.ReturnMessage && 
+                this.processApexReturnValue.includes('No record found against given input.')) {
+                this.showToast('Invalid IFSC Code, No record found against given input.', 'warning');
+       }
             if (this.processApexReturnValue.ReturnCode == "0") {
                 const nsdlData = this.processApexReturnValue.lstDetails;
                 this.nsdlResponse = nsdlData;
