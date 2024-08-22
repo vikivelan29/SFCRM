6fff6a32a3b7a501759155a7b70af9f0ef734fb9
Kuldeep Mishra <kuldeep.mishra@salesforce.com>
2024-08-22 06:58:28 +0000

    FALevelDetails

--- force-app/main/default/lwc/abhi_FALevelDetails/abhi_FALevelDetails.js
+++ force-app/main/default/lwc/abhi_FALevelDetails/abhi_FALevelDetails.js
@@ -22,11 +22,24 @@ export default class Abhil_FALevelDetails extends LightningElement {
     get isSearchDisabled() {
         if (!this.startDate || !this.endDate) {
             return true; // Disable if either date is empty
-        }        
-        //const start = new Date(this.startDate);
-        //const end = new Date(this.endDate);
-        //return end <= start; 
+        }
+        return new Date(this.startDate) > new Date(this.endDate); // Disable if start date is greater than end date
+        
+        
   }
+
+  get DateErrorMessage() {
+    if (!this.startDate || !this.endDate) {
+        //return 'Both dates must be provided.';
+    }
+    if (new Date(this.startDate) > new Date(this.endDate)) {
+        return 'Please select a valid date range.';
+    }
+    return '';
+}
+// get isDataTableDisabled() {
+//     return !!this.DateErrorMessage; // Disable the data table if there's an error
+// }
 
     showRecords = false;
     label = {
