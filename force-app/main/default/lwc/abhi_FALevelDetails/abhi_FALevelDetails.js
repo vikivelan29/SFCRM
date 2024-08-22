00e46076125c9677c2e2f7685f21ee41760a7e8b
Kuldeep Mishra <kuldeep.mishra@salesforce.com>
2024-08-22 09:17:15 +0000

    FALevelDetails

--- force-app/main/default/lwc/abhi_FALevelDetails/abhi_FALevelDetails.js
+++ force-app/main/default/lwc/abhi_FALevelDetails/abhi_FALevelDetails.js
@@ -22,10 +22,11 @@ export default class Abhil_FALevelDetails extends LightningElement {
     get isSearchDisabled() {
         if (!this.startDate || !this.endDate) {
             return true; // Disable if either date is empty
-        }        
-        //const start = new Date(this.startDate);
-        //const end = new Date(this.endDate);
-        //return end <= start; 
+        }
+        //return new Date(this.startDate) > new Date(this.endDate); // Disable if start date is greater than end date
+        const start = new Date(this.startDate);
+        const end = new Date(this.endDate);
+        return end < start; 
   }
 
     showRecords = false;
@@ -141,6 +142,10 @@ showNotification(title, message, variant) {
         variant: variant,
     });
     this.dispatchEvent(event);
+}
+
+get isEndDateDisabled() {
+    return !this.startDate;
 }
 
 }
