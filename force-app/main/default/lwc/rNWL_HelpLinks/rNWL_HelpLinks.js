95ee4e6e1da6b7eb9f424561fc07ed3f2481359a
Joshna Reddy Palla <jpalla@salesforce.com>
2024-10-14 13:52:04 +0000

    Deploying changes

--- force-app/main/default/lwc/rNWL_HelpLinks/rNWL_HelpLinks.js
+++ force-app/main/default/lwc/rNWL_HelpLinks/rNWL_HelpLinks.js
@@ -1,21 +1,12 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement } from 'lwc';
 import RNWL_Seller_Portal_Link from '@salesforce/label/c.RNWL_Seller_Portal_Link';
 import RNWL_Product_Features from '@salesforce/label/c.RNWL_Product_Features';
-import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
-import PAYMENT_LINK from "@salesforce/schema/Opportunity.Payment_Link__c";
+import RNWL_Auto_Calculation from '@salesforce/label/c.RNWL_Auto_Calculation';
 
 export default class RNWL_HelpLinks extends LightningElement {
-    @api recordId;
-    autoCalculationLink = '';
     label = {
         RNWL_Seller_Portal_Link,
-        RNWL_Product_Features
+        RNWL_Product_Features,
+        RNWL_Auto_Calculation
     };
-
-    @wire(getRecord, {recordId: "$recordId", fields: [PAYMENT_LINK]})
-    oppRecord;
-
-    get paymentLink() {
-        return getFieldValue(this.oppRecord.data, PAYMENT_LINK);
-    }
 }
