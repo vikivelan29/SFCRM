9c3b9d97d793db730dd2c83740b47946a70abf90
Joshna Reddy Palla <jpalla@salesforce.com>
2024-10-15 09:19:08 +0000

    com_2

--- force-app/main/default/lwc/rNWL_NonIndividualAccRenewalDetails/rNWL_NonIndividualAccRenewalDetails.js
+++ force-app/main/default/lwc/rNWL_NonIndividualAccRenewalDetails/rNWL_NonIndividualAccRenewalDetails.js
@@ -61,10 +61,10 @@ export default class RNWL_NonIndividualAccRenewalDetails extends LightningElemen
             sectionLabel : 'Premium Details' , 
             headres : [
                 { label: 'Certificate Number',      fieldName: 'Certificate_number' , wrapText: true},
-                { label: 'Upsell Net Premium',      fieldName: 'Upsell_Net_Premium' , wrapText: true , type: 'currency'},
-                { label: 'Upsell Gross Premium',    fieldName: 'Upsell_Gross_Premium' , wrapText: true , type: 'currency'},
-                { label: 'Renewal Net Premium',     fieldName: 'Renewal_Net_Premium' , wrapText: true , type: 'currency'},
-                { label: 'Renewal Gross Premium',   fieldName: 'Renewal_Gross_Premium', wrapText: true , type: 'currency'},
+                { label: 'Upsell Net Premium',      cellAttributes: { alignment: 'left' }, fieldName: 'Upsell_Net_Premium' , wrapText: true , type: 'currency'},
+                { label: 'Upsell Gross Premium',    cellAttributes: { alignment: 'left' }, fieldName: 'Upsell_Gross_Premium' , wrapText: true , type: 'currency'},
+                { label: 'Renewal Net Premium',     cellAttributes: { alignment: 'left' }, fieldName: 'Renewal_Net_Premium' , wrapText: true , type: 'currency'},
+                { label: 'Renewal Gross Premium',   cellAttributes: { alignment: 'left' }, fieldName: 'Renewal_Gross_Premium', wrapText: true , type: 'currency'},
             ],
             records : jsonData.premiumDetails,
             sectionAvailable : jsonData.premiumDetails.length > 0,
@@ -81,11 +81,11 @@ export default class RNWL_NonIndividualAccRenewalDetails extends LightningElemen
                 { label: 'Email',               fieldName: 'Email' , wrapText: true},
                 { label: 'Mobile Number',       fieldName: 'Mobile_Number' , wrapText: true},
                 { label: 'Relation',            fieldName: 'Relation' , wrapText: true},
-                { label: 'Sum Insured',         fieldName: 'SumInsured' , wrapText: true , type: 'currency'},
-                { label: 'Net Premium',         fieldName: 'NetPremium' , wrapText: true , type: 'currency'},
-                { label: 'New Premium U',       fieldName: 'NetPremium_U' , wrapText: true , type: 'currency'},
-                { label: 'CB',                  fieldName: 'CB' , wrapText: true , type: 'currency'},
-                { label: 'HR Amount',           fieldName: 'Hr_Amount' , wrapText: true , type: 'currency'},
+                { label: 'Sum Insured',         cellAttributes: { alignment: 'left' }, fieldName: 'SumInsured' , wrapText: true , type: 'currency'},
+                { label: 'Net Premium',         cellAttributes: { alignment: 'left' }, fieldName: 'NetPremium' , wrapText: true , type: 'currency'},
+                { label: 'New Premium U',       cellAttributes: { alignment: 'left' }, fieldName: 'NetPremium_U' , wrapText: true , type: 'currency'},
+                { label: 'CB',                  cellAttributes: { alignment: 'left' }, fieldName: 'CB' , wrapText: true , type: 'currency'},
+                { label: 'HR Amount',           cellAttributes: { alignment: 'left' }, fieldName: 'Hr_Amount' , wrapText: true , type: 'currency'},
             ],
             records : jsonData.memberDetails,
             sectionAvailable : jsonData.memberDetails.length > 0,
@@ -135,7 +135,7 @@ export default class RNWL_NonIndividualAccRenewalDetails extends LightningElemen
             ],
             records : jsonData.combiDetails,
             sectionAvailable : true,
-            message : 'No combi-policies to display'
+            message :  jsonData.combiDetails && jsonData.combiDetails.length == 0 ? 'No combi-policies to display' : ''
         };
 
         this.data.push (policyDetailSections, 
