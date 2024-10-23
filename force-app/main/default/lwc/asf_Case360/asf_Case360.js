0c4113f6885ac2af518da9975c31ee1981c546c3
AnirudhRaturi <anirudh.raturi@salesforce.com>
2024-10-23 13:04:20 +0530

    Change by Anirudh

--- force-app/main/default/lwc/asf_Case360/asf_Case360.js
+++ force-app/main/default/lwc/asf_Case360/asf_Case360.js
@@ -2958,6 +2958,7 @@ export default class Asf_Case360 extends NavigationMixin(LightningElement) {
                 this.template.querySelectorAll('lightning-input-field').forEach(ele => {
                     if(ele.fieldName == fieldApiNm){
                         ele.value = fieldVal;
+                        ele.focus();
                     }
                 });
             }
