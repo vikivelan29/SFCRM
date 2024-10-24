30c03cb6bb9debf627406f913ccb4b60bdc3bc24
Aditya Jawadekar <aditya.jawadekar@salesforce.com>
2024-10-24 12:35:11 +0000

    omnidocs changes

--- force-app/main/default/lwc/aBHI_OmniDocViewAccordianForCase/aBHI_OmniDocViewAccordianForCase.js
+++ force-app/main/default/lwc/aBHI_OmniDocViewAccordianForCase/aBHI_OmniDocViewAccordianForCase.js
@@ -1,6 +1,7 @@
 import { LightningElement, api } from 'lwc';
 
 export default class ABHI_OmniDocViewAccordianForCase extends LightningElement {
+    
     openSections = [];
     @api recordId;
     @api objectApiName;
