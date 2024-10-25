753c14731dd41febffddfb83bc04d8c3b166d2ec
jadalanaveen <naveen.jadala@blue5green.com>
2024-10-25 11:02:39 +0530

    Update asf_CurrentHoldings.js

--- force-app/main/default/lwc/asf_CurrentHoldings/asf_CurrentHoldings.js
+++ force-app/main/default/lwc/asf_CurrentHoldings/asf_CurrentHoldings.js
@@ -125,7 +125,7 @@ export default class Asf_CurrentHoldings extends LightningElement {
         let allowCreateCase = true;
 
         // Add changes for Product listing LWC i.e asf_FetchAssetsRelatedToAccount
-        if(this.infoObject != undefined && this.infoObject.hasOwnProperty("isAsset")) {
+        if(this.infoObject.hasOwnProperty("isAsset")) {
             let isAsset = this.infoObject.isAsset;
             if(isAsset == "false") {
                 this.withoutAsset = true;
@@ -135,17 +135,14 @@ export default class Asf_CurrentHoldings extends LightningElement {
             }
         }
 
-        // Added validation for ABHI BU when no asset is selected in overview component on click of Create Case Button
-        if(this.infoObject != undefined && this.infoObject.hasOwnProperty("businessUnit")) {
+        if(this.infoObject.hasOwnProperty("businessUnit")) {
             let businessUnit = this.infoObject.businessUnit;
             if(businessUnit == 'ABHI' && this.withoutAsset){
                 allowCreateCase = false;
                 this.showNotification("Error", this.ABHI_PolicyRequiredErrMsgLabel, 'error');
             }
         }
-        if(allowCreateCase) {
-            this.showCreateCaseModal = true;
-        }
+        if(allowCreateCase) this.showCreateCaseModal = true;
 
     }
 
