89f2c5f62b6b4aed3b7eb6f221acb00a15c5b224
Aditya Jawadekar <aditya.jawadekar@salesforce.com>
2024-11-04 10:07:28 +0000

    PR1030924-701 -- changes

--- force-app/main/default/lwc/aBHI_LegacyCases/aBHI_LegacyCases.js
+++ force-app/main/default/lwc/aBHI_LegacyCases/aBHI_LegacyCases.js
@@ -23,7 +23,6 @@ export default class Abfl_LegacyCases extends LightningElement {
     displayError = false;
     showChildTable = false;
     selectedAsset;
-    selectedFinalAsset;
     leagcyCaseData;
     startDate;
     endDate;
@@ -93,7 +92,6 @@ export default class Abfl_LegacyCases extends LightningElement {
         fetchAssets({accRec: this.recordId})
         .then(result => {
             this.options = result;
-            this.options = [{label: 'None', value: ''}, ...result];
         })
         .catch(error => {
             console.error("Error", error);
@@ -104,24 +102,11 @@ export default class Abfl_LegacyCases extends LightningElement {
         fetchMembers({strRecordId: this.recordId})
         .then(result => {
             this.memberOptions = result;
-            this.memberOptions = [{label: 'None', value: ''}, ...result];
-            this.selectedMember = getFieldValue(this.account.data, CLIENT_CODE_FIELD);
+            //this.selectedMember = result[0].value;
         })
         .catch(error => {
             console.error("Error", error);
         })
-    }
-
-    resetSearch() {
-        this.selectedMember = '';
-        this.selectedFinalAsset = '';
-        this.msdCaseNumber = '';
-        this.phoneNo = '';
-        this.emailId = '';
-        this.startDate = '';
-        this.endDate = '';
-        this.selectedAsset = '';
-        this.arcPolicyNo = '';
     }
 
     //Callout to get Legacy data
@@ -132,12 +117,9 @@ export default class Abfl_LegacyCases extends LightningElement {
         this.customerId = this.selectedMember;
         this.lob = getFieldValue(this.account.data, LOB_FIELD);
         console.log('$.$.Acc'+this.lob);
-        if(!this.selectedMember && !this.phoneNo && !this.emailId){
-            this.selectedMember = getFieldValue(this.account.data, CLIENT_CODE_FIELD);
-        }
         this.mapRequest = {
             "CustomerNumber": this.selectedMember,
-            "PolicyNumber": this.selectedFinalAsset,
+            "PolicyNumber": this.selectedAsset,
             "FromDate": this.startDate,
             "ToDate": this.endDate,
             "LOB": this.lob,
@@ -188,7 +170,6 @@ export default class Abfl_LegacyCases extends LightningElement {
 
         this.selectedAsset = event.detail.value;
         this.arcPolicyNo = '';
-        this.selectedFinalAsset = event.detail.value;
         console.log('this.sa'+JSON.stringify(event.detail));
     }
 
@@ -196,29 +177,24 @@ export default class Abfl_LegacyCases extends LightningElement {
 
         this.selectedAsset = '';
         this.arcPolicyNo = event.detail.value;
-        this.selectedFinalAsset = event.detail.value;
         console.log('this.ap'+JSON.stringify(event.detail));
     }
 
     handleEmailChange(event) {
 
         this.emailId = event.detail.value;
-        this.selectedMember = '';
         console.log('this.em'+JSON.stringify(event.detail));
     }
 
     handlePhoneChange(event) {
 
         this.phoneNo = event.detail.value;
-        this.selectedMember = '';
         console.log('this.ph'+JSON.stringify(event.detail));
     }
 
     handleMemberChange(event) {
 
         this.selectedMember = event.detail.value;
-        this.emailId = '';
-        this.phoneNo = '';
         console.log('this.sm'+JSON.stringify(event.detail));
     }
 
