3b4d57a8046e264277256dd6f2623e3d70ed2294
abhijeetambwade-github <169041120+abhijeetambwade-github@users.noreply.github.com>
2024-12-02 18:15:38 +0530

    Update asf_ConstantUtility.js

--- force-app/main/default/lwc/asf_ConstantUtility/asf_ConstantUtility.js
+++ force-app/main/default/lwc/asf_ConstantUtility/asf_ConstantUtility.js
@@ -323,7 +323,7 @@ export const lanLabels = {
     ABHI: {
         CTST_COLS: [
             { label: 'Nature', fieldName: 'Nature__c', type: 'text' },
-            { label: 'LOB', fieldName: 'LOB__c', type: 'text' },
+           // { label: 'LOB', fieldName: 'LOB__c', type: 'text' },
             { label: 'Type', fieldName: 'Type__c', type: 'text' },
             { label: 'Sub Type', fieldName: 'Sub_Type__c', type: 'text' }
         ],
@@ -390,10 +390,11 @@ export const lanLabels = {
       //Added by Praneeth for CTST columns of Case Category for ABML
       ABML: {
         
+        CREATE_CASE_WITH_FA: "Products not available for ABML",
         PRODUCT_SEARCH_PLACEHOLDER: "Enter Customer Name /Client Code /Email /Mobile /LAN# /PAN#",
         CASE_ELIGIBLE_WITH_NEW_CTST_MSG: "Current Case Type for this case, is eligible for this Customer and LAN combination. Do you want to proceed with existing Case Type Sub Type or Re-Categorize Type Subtype?",
         CASE_NOT_ELIGIBLE_WITH_EXISING_CST_MSG : 'Current Case Type for this case, is not eligible for this Customer and LAN combination. We recommend selecting the right Case Type Sub Type for the case ?',
-        CUSTOMER_TAGGING_CARD_TITLE: "Customer/Prospect/LAN Tagging",
+        CUSTOMER_TAGGING_CARD_TITLE: "Customer/Prospect Tagging",
         CTST_COLS: [
             { label: 'Nature', fieldName: 'Nature__c', type: 'text' },
             { label: 'Type', fieldName: 'Type__c', type: 'text' },
