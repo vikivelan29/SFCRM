00e417b86904e98db5a3b39221703c8feefdaefe
abhijeetambwade-github <169041120+abhijeetambwade-github@users.noreply.github.com>
2024-12-30 14:50:23 +0530

    Update abhfl_GenericRepeatingAndOpenComplaintCase.js
    JS update for Litmus

--- force-app/main/default/lwc/abhfl_GenericRepeatingAndOpenComplaintCase/abhfl_GenericRepeatingAndOpenComplaintCase.js
+++ force-app/main/default/lwc/abhfl_GenericRepeatingAndOpenComplaintCase/abhfl_GenericRepeatingAndOpenComplaintCase.js
@@ -1,43 +1,40 @@
-import {
-    LightningElement,
-    api,
-    track,
-    wire
-} from 'lwc';
-import {
-    getRecord,
-    getFieldValue
-} from 'lightning/uiRecordApi';
-//import { WorkspaceApi } from 'lightning/workspaceApi';
-
-import { CurrentPageReference } from 'lightning/navigation';
+import { LightningElement, api, track, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import Days_For_Repeated_Indicator from '@salesforce/label/c.Repeated_Indicator';
 import getRecords from '@salesforce/apex/Abhfl_GenericRepeatingAndOpenComplntClas.genericFetchQuery';
 import getCaseCounts from '@salesforce/apex/Asf_NpsIndicatiorController.getCaseCounts';
 import getNpsScore from '@salesforce/apex/Asf_NpsIndicatiorController.getNpsScore';
 import BUSINESS_UNIT from '@salesforce/schema/Account.Business_Unit__c';
+import { lanLabels } from 'c/asf_ConstantUtility';
 import loggedInUserId from '@salesforce/user/Id';
 import UserBusinessUnit from '@salesforce/schema/User.Business_Unit__c';
-import { EnclosingTabId, getTabInfo, openSubtab } from 'lightning/platformWorkspaceApi';
-import {
-    NavigationMixin
-} from 'lightning/navigation';
 import getSurveyResponseFieldsByAccountId from '@salesforce/apex/Abhfl_GenericRepeatingAndOpenComplntClas.getSurveyResponseFieldsByAccountId';
+import { CurrentPageReference } from 'lightning/navigation';
+import { EnclosingTabId, getTabInfo, openSubtab } from 'lightning/platformWorkspaceApi';
+import { NavigationMixin} from 'lightning/navigation';
 export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends NavigationMixin(LightningElement) {
-    @track isDisplayVisible = false; // added by nikhil
-    showchild = false; // added by nikhil
+
     @api recordId;
     @api objectApiName;
-    isAbhfl;;
+    isAbhfl;
+    isWellness;
+    isOther;
     @track caseRecord;
     @track records;
     @track fieldArr = 'id';
-    
 
-    
+    @track surveyresponse = [];
+    @track surveyresponse1 = [];
+    @track surveyresponse2 = [];
+    @track columns = [];
+    @track columns1 = [];
+    @track columns2 = [];
+    @track isrespdataaval = false;
+    @track isloading = false;
     @track showdetailsbuttonhide = false;
     @track loggedInUserBusinessUnit = '';
     errorMessage;
+
     whereClauseForRI = '';
     whereClauseForOC = '';
     riFlag;
@@ -50,14 +47,14 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Navigati
     ocFlagNps;
     isLoadedNps;
     showOpenCase = "⚪️";
-    showEscalatedCases = 0;
+    showEscalatedCases=0;
     nps = undefined;
     isAccount = false;
-    workspaceAPI
+    showCustomerNPSbyNumber;
+    customerBU = '';
 
-    @wire(EnclosingTabId) tabId;
-    @wire(CurrentPageReference) pageRef;
-    @wire(getRecord, {
+
+     @wire(getRecord, {
         recordId: loggedInUserId,
         fields: [UserBusinessUnit]
     })
@@ -77,41 +74,78 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Navigati
         }
     }
     loadNpsScore() {
-        getNpsScore({
-                customerId: this.recordId
-            })
+        getNpsScore({ customerId: this.recordId })
             .then(result => {
                 this.nps = result;
-                console.log('NPS record', this.nps);
+                console.log('NPS record', this.nps); 
+                this.claculateNPSRating();
             })
             .catch(error => {
                 console.error('Error loading NPS record', error);
             });
     }
-    get showCustomerNPSbyNumber() {
-        if (this.nps == undefined) {
-            return "❌";
-        } else if (this.nps >= 0 && this.nps <= 6) {
-            return "🙁";
-        } else if (this.nps > 6 && this.nps <= 8) {
-            return "😐";
-        } else if (this.nps > 8 && this.nps <= 10) {
-            return "😁";
-        } else {
-            return this.nps;
+
+    claculateNPSRating() {
+
+        this.showCustomerNPSbyNumber = undefined;
+
+        if(JSON.stringify(this.nps) !== "{}") {
+            this.businessUnit = Object.keys(this.nps)[0];
+            this.showCustomerNPSbyNumber = this.nps[this.businessUnit];
+        }
+        else {
+            this.businessUnit = this.customerBU;
+        }
+
+        if(this.businessUnit && (this.businessUnit !== lanLabels[this.businessUnit].ABHI_BUSINESS_UNIT)) {
+            if (this.showCustomerNPSbyNumber == 0 || this.showCustomerNPSbyNumber == undefined) {
+                this.showCustomerNPSbyNumber =  "❌";
+        }
+            else if(this.showCustomerNPSbyNumber > 0 && this.showCustomerNPSbyNumber <= 3){
+                this.showCustomerNPSbyNumber =  "🙁";
+            }
+            else if(this.showCustomerNPSbyNumber > 3 &&  this.showCustomerNPSbyNumber <= 6){
+                this.showCustomerNPSbyNumber =  "😐";
+        }
+            else if(this.showCustomerNPSbyNumber > 6 && this.showCustomerNPSbyNumber <= 10){
+                this.showCustomerNPSbyNumber =  "😁";
+            }
+            else {
+                this.showCustomerNPSbyNumber = '';
+            }
+        }
+        else if(this.businessUnit && (this.businessUnit === lanLabels[this.businessUnit].ABHI_BUSINESS_UNIT)) {
+            this.logicToShowEmoji();
         }
     }
+
+    logicToShowEmoji() {
+        if(this.showCustomerNPSbyNumber <= 6){
+            this.showCustomerNPSbyNumber =  "🙁";
+        }
+        else if(this.showCustomerNPSbyNumber <= 8){
+            this.showCustomerNPSbyNumber =  "😐";
+        }
+        else if(this.showCustomerNPSbyNumber <= 10){
+            this.showCustomerNPSbyNumber =  "😁";
+        }
+        else {
+            this.showCustomerNPSbyNumber = '';
+        }
+    }
+
     @wire(getRecord, {
         recordId: '$recordId',
         fields: [BUSINESS_UNIT]
     })
-    wiredAccount({
-        error,
-        data
-    }) {
+    wiredAccount({ error, data }) {
         if (data) {
             const businessUnitValue = getFieldValue(data, BUSINESS_UNIT);
             this.isAbhfl = businessUnitValue === 'ABHFL';
+            this.isWellness = businessUnitValue === 'Wellness';
+            this.isOther = (businessUnitValue !== 'ABHFL' && businessUnitValue !== 'Wellness');
+            this.customerBU = businessUnitValue ?? '';
+            this.loadNpsScore();
         } else if (error) {
             console.error('Error occured in  retrieving business unit', error);
         }
@@ -130,7 +164,6 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Navigati
     connectedCallback() {
         this.getCaseRecord();
         this.getCaseRecordNps();
-        this.loadNpsScore();
     }
 
     async getCaseRecord() {
@@ -141,24 +174,22 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Navigati
             let fieldArray = "id, AccountId, LAN__c, Nature__c, Type_Text__c, Sub_Type_Text__c";
             let whereClauseOfRI = "WHERE id ='" + this.recordId + "' WITH SECURITY_ENFORCED";
 
-            let rec = await getRecords({
-                fields: fieldArray,
-                objectName: this.objectApiName,
-                whereClause: whereClauseOfRI
-            });
+            let rec = await getRecords({ fields: fieldArray, objectName: this.objectApiName, whereClause: whereClauseOfRI });
             if (rec) {
                 this.caseRecord = rec[0];
                 let caseAccId = this.caseRecord.hasOwnProperty('AccountId') ? this.caseRecord.AccountId : '';
-                if (caseAccId) {
+                if(caseAccId) {             
                     if (!this.whereClauseForRI && !this.whereClauseForOC && caseAccId) {
                         this.initializeWhereClause();
-                    }
-                    this.story_328_329_330();
-                } else {
+                    } 
+                    this.story_328_329_330();              
+                }
+                else {
                     this.isLoaded = false;
                 }
             }
-        } else if (this.objectApiName == 'Account') {
+        }
+        else if (this.objectApiName == 'Account') {
             this.isAccount = true;
             this.addIconClass("[data-id='Repeated_Indicator']", 'slds-hide');
             this.removeIconClass("[data-id='Open_Complaint_Indicator_div']", 'slds-hide');
@@ -168,7 +199,7 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Navigati
             this.objectApiName = 'Account';
 
         }
-
+        
 
     }
 
@@ -176,7 +207,8 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Navigati
         let caseNature = this.caseRecord.Nature__c;
         if (caseNature == "Complaint") {
             this.story_328_329();
-        } else {
+        }
+        else {
             this.addIconClass("[data-id='Open_Complaint_Indicator_div']", 'slds-hide');
         }
         this.story_330();
@@ -194,23 +226,21 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Navigati
                                  ${withSecEnforced} LIMIT 2`;
 
             this.whereClauseForOC = `${commonForOC} AND AccountId = \'${this.caseRecord.AccountId}\'  ${withSecEnforced} LIMIT 1`;
-        } else if (this.objectApiName == 'Account') {
+        }
+        else if (this.objectApiName == 'Account') {
             this.whereClauseForOC = `${commonForOC} AND AccountId = \'${this.recordId}\'  ${withSecEnforced} LIMIT 1`;
         }
     }
 
     async story_328_329() {
         let iconAttrObjOC = {};
-        let sobRecords = await getRecords({
-            fields: this.fieldArr,
-            objectName: this.objectApiName,
-            whereClause: this.whereClauseForOC
-        });
+        let sobRecords = await getRecords({ fields: this.fieldArr, objectName: this.objectApiName, whereClause: this.whereClauseForOC });
         iconAttrObjOC.dataId = '[data-id="Open_Complaint_Indicator"]';
         if (sobRecords && sobRecords.length > 0) {
             iconAttrObjOC.variant = 'error';
             this.addAndChangeAttributes(iconAttrObjOC);
-        } else {
+        }
+        else {
             iconAttrObjOC.variant = '';
             this.addAndChangeAttributes(iconAttrObjOC);
         }
@@ -220,11 +250,7 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Navigati
 
     async story_330() {
         let iconAttrObjRI = {};
-        let sobRecords = await getRecords({
-            fields: this.fieldArr,
-            objectName: this.objectApiName,
-            whereClause: this.whereClauseForRI
-        });
+        let sobRecords = await getRecords({ fields: this.fieldArr, objectName: this.objectApiName, whereClause: this.whereClauseForRI });
         if (sobRecords && sobRecords.length > 0) {
             iconAttrObjRI.dataId = '[data-id="Repeated_Indicator"]';
             iconAttrObjRI.variant = 'error';
@@ -235,7 +261,7 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Navigati
 
     addAndChangeAttributes(attrbObj) {
         let getLightningIcon = this.template.querySelector(attrbObj.dataId);
-        //  getLightningIcon.variant = attrbObj.variant;
+        getLightningIcon.variant = attrbObj.variant;
     }
 
     addIconClass(dataId, iconClass) {
@@ -261,9 +287,7 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Navigati
         this.npsIndicator();
     }
     async npsIndicator() {
-        await getCaseCounts({
-                accountId: this.recordId
-            })
+        await getCaseCounts({ accountId: this.recordId })
             .then((result) => {
                 this.isLoaded = false;
                 if (result) {
@@ -280,13 +304,87 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Navigati
                 console.error(error);
             });
     }
-    getCaseRecordCommon() {
+    getCaseRecordCommon(){
         this.loadNpsScore();
         this.getCaseRecord();
         this.getCaseRecordNps();
     }
+    // Added By Yogesh start[PR970457-2195]     
+     async handleClick() {
+        console.log('I m here');
+        // this.getCaseRecordCommon();
+        this.isloading = true;
+        await getSurveyResponseFieldsByAccountId({
+                accountId: this.recordId
+            })
+            .then(data => {
+                this.isloading = false;
+                this.isrespdataaval = true;
+                this.columns = data.columnwrap;
+                this.columns1 = data.columnwrap1;
+                this.columns2 = data.columnwrap2;
+                this.surveyresponse = data.rowdata;
+                this.surveyresponse1 = data.rowdata1;
+                this.surveyresponse2 = data.rowdata2;
+                console.log('data-->' + JSON.stringify(data));
+                this.mapPicklistOptionsToRows(this.surveyresponse, data.columnwrap);
+                this.mapPicklistOptionsToRows(this.surveyresponse1, data.columnwrap1);
+                this.mapPicklistOptionsToRows(this.surveyresponse2, data.columnwrap2);
+                //this.setupColumns(data);
+                //this.error = undefined;
+            })
+            .catch(error => {
+                //this.error=true;
+                this.isloading = false;
+                console.log('errror-->' + JSON.stringify(error));
+                this.errorMessage = error.body.message;
+            });
+    }
 
-    // Added By Yogesh start[PR970457-2195]
+    mapPicklistOptionsToRows(rows, columnwrap) {
+        
+        rows.forEach(row => {
+            columnwrap.forEach(column => {
+                console.log('column:::', JSON.stringify(column))
+                console.log('row:::', JSON.stringify(row))
+                let picklistField = column.fieldName; 
+                if (column.options && column.fieldName) {
+                    // Find the corresponding picklist label based on the API name
+                    const picklistOptions = column.options;
+                    let selectedValue = ''
+                    if (picklistField.includes('Case__r.')) {
+                        if (row['Case__r'] != undefined && row['Case__r'][picklistField.split('.')[1]] != undefined)
+                            selectedValue = row['Case__r'][picklistField.split('.')[1]];
+                    } else {
+                        selectedValue = row[picklistField];
+                    }
+                    const selectedOption = picklistOptions.find(option => option.value === selectedValue);
+
+                    if (selectedOption) {
+                        // Replace the API name with the label
+                        row[picklistField] = selectedOption.label;
+                    }
+                } else {
+                    let selectedValue = ''
+                    if (picklistField.includes('Case__r.')) {
+                        if (row['Case__r'] != undefined && row['Case__r'][picklistField.split('.')[1]] != undefined)
+                            selectedValue = row['Case__r'][picklistField.split('.')[1]];
+                    } else {
+                        selectedValue = row[picklistField];
+                    }
+                    if (selectedValue) {
+                        row[picklistField] = selectedValue;
+                    }
+                }
+            });
+        })
+    }
+
+    closeresp() {
+        this.isrespdataaval = false;
+    }
+	
+	// Added By Yogesh start[PR970457-2195]
         async handleClick() {
         try {
             // Get tab info to find the primary tab
@@ -375,6 +473,4 @@ export default class Abhfl_GenericRepeatingAndOpenComplaintCase extends Navigati
         window.dispatchEvent(apiEvent);
         });
     }
-    
-    
 }
