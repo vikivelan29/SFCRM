fcf6b9250b8d65e8fe257886458a43a8ff34dd80
abhijeetambwade-github <169041120+abhijeetambwade-github@users.noreply.github.com>
2024-12-30 17:52:44 +0530

    Update asf_RecategoriseCase.js
    issue fix

--- force-app/main/default/lwc/asf_RecategoriseCase/asf_RecategoriseCase.js
+++ force-app/main/default/lwc/asf_RecategoriseCase/asf_RecategoriseCase.js
@@ -755,7 +755,7 @@ export default class asf_RecategoriseCase extends NavigationMixin(LightningEleme
             notifyRecordUpdateAvailable(changeArray);
             this.isNotSelected = true;
             this.createCaseWithAll = false; 
-            //window.location.reload();
+            window.location.reload(); //PR970457-2762 fix
         })
         .catch(error => {
             console.log(error);
