706ae0fc27c5ecba38fdb34d0dfa56e9b587c4eb
sid-jaitly <sjaitly@salesforce.com>
2025-02-14 13:48:55 +0530

    Added Last Premium Payment Date for Health Insurance

--- force-app/main/default/lwc/abcdHealthInsuranceDetails/abcdHealthInsuranceDetails.js
+++ force-app/main/default/lwc/abcdHealthInsuranceDetails/abcdHealthInsuranceDetails.js
@@ -75,17 +75,16 @@ const columns = [
                         label: 'Policy Quote Number',
                         type: 'text'
                     },
-                    /* Last Premium Payment Date not present from ABCD, and hence commenting.
                     {
-                        fieldName: '',
+                        fieldName: 'lastPremiumPaymentDate',
                         label: 'Last Premium payment date',
                         type: 'text',
-                        /*typeAttributes: {
+                        typeAttributes: {
                             day: "2-digit",
                             month: "2-digit",
                             year: "numeric"
                         }
-                    },*/
+                    },
                     {
                         fieldName: 'annualPremium',
                         label: 'Annual Premium',
