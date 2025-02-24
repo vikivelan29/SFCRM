ee3d782afc90b8d3655fc41ebf8107002f4f76f5
dhineshkumar.teekara <dhineshkumar.teekara@petronas.com>
2025-02-24 13:15:19 +0530

    ABCD VAS Dynamic Error Messages

--- force-app/main/default/lwc/abcdLoyaltyRewardDetails/abcdLoyaltyRewardDetails.js
+++ force-app/main/default/lwc/abcdLoyaltyRewardDetails/abcdLoyaltyRewardDetails.js
@@ -1,5 +1,27 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement,api,track } from 'lwc';
 
 export default class AbcdLoyaltyRewardDetails extends LightningElement {
     @api loyaltyrewards;
+        @track showPopup = false;
+    
+
+    openTransactionPopUp(event){
+        this.showPopup = true;
+    }
+
+    closePopup() {
+        this.showPopup = false;
+    }
+    @api status;
+    @api apiMessage;
+    get displayError(){
+        if(this.status != 'Success'){
+            return true;
+        }
+        return false;
+    }
+
+    get displayNoData() {
+        return this.status === 'Success' && (!this.loyaltyrewards || this.loyaltyrewards.length === 0 || this.loyaltyrewards == null);
+    }
 }
