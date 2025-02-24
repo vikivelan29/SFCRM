89243ff62b74e4c181d823627512c63c4218c975
Shubham Sinha <shubham.sinha@salesforce.com>
2025-02-24 07:45:39 +0000

    abcdLoylatyTransactionHistory

---
+++ force-app/main/default/lwc/abcdLoylatyTransactionHistory/abcdLoylatyTransactionHistory.js
@@ -0,0 +1,163 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+import getTrasanctionDetails from '@salesforce/apex/ABCD_DGTransactionHistoryController.getTransactions';
+
+export default class AbcdLoylatyTransactionHistory extends LightningElement {
+    @api mobileNumber;
+    @track customerID;  
+    @track selectedOption = 'last5'; 
+    @track fromDate = '';
+    @track toDate = '';
+    @track showDateFields = false;
+    @track isModalOpen = false;
+    @track transactionDetails = [];
+    showTransactionTable = false;
+    @track currentPage = 1;
+    @track pageSize = 5;
+    @api clientid;
+    @api emailId; 
+    
+
+    get options() {
+        return [
+            { label: 'Last 5 transactions', value: 'last5' },
+            { label: 'Date Filter', value: 'custom' }
+        ];
+    }
+    
+    handleMobileChange(event){
+        this.mobileNumber = event.target.value;
+    }
+
+    handleOptionChange(event) {
+        this.selectedOption = event.detail.value;
+        this.showDateFields = this.selectedOption === 'custom';
+        if (!this.showDateFields) {
+            this.fromDate = '';
+            this.toDate = '';
+        }
+    }
+    
+    handleDateChange(event) {
+        const field = event.target.name;
+        this[field] = event.target.value;
+        this.validateDates();
+    }
+    
+    validateDates() {
+        const today = new Date().toISOString().split('T')[0];
+        if (this.fromDate && this.fromDate > today) {
+            this.showToast('Error', 'From Date cannot be greater than today.', 'error');
+            this.fromDate = '';
+        }
+        if (this.toDate && this.toDate < this.fromDate) {
+            this.showToast('Error', 'To Date cannot be earlier than From Date.', 'error');
+            this.toDate = '';
+        }
+    }
+    
+    showToast(title, message, variant) {
+        this.dispatchEvent(new ShowToastEvent({ title, message, variant }));
+    }
+    
+    get isButtonVisible() {
+        return this.selectedOption === 'last5' || (this.fromDate && this.toDate);
+    }
+    
+    handleClear() {
+        this.selectedOption = 'last5';
+        this.showDateFields = false;
+        this.fromDate = '';
+        this.toDate = '';
+        this.transactionDetails = [];
+        this.showTransactionTable = false;
+        this.currentPage = 1;
+    }
+
+    @api openModal(recordId) {
+        if (recordId) {
+            this.recordId = recordId;
+            this.isModalOpen = true;
+        }
+    }
+    
+    closeModal() {
+        this.isModalOpen = false;
+    }
+
+    async getTransaction(){
+        try {
+                const result = await getTrasanctionDetails({
+                    mobileNumber: this.mobileNumber,
+                    customerID : this.clientid,
+                    emailID : this.emailId,
+                    startDate: this.selectedOption === 'last5' ? null : this.fromDate,
+                    toDate: this.selectedOption === 'last5' ? null : this.toDate
+                });
+                if (result.isSuccess) {
+                    debugger
+                    this.showTransactionTable = true;
+                    console.log('Fetched Transactions:', JSON.stringify(result.dgTransactionDetails));
+                    let transactions = result.dgTransactionDetails.map(txn => ({
+                        transactionType: txn.transactionType,
+                        amount: txn.amount,
+                        unit: txn.unit,
+                        transactionDate: txn.transactionDate,
+                        transactionStatus: txn.transactionStatus
+                    }));
+
+                    if (this.selectedOption === 'last5') {
+                        transactions = transactions.slice(-5); // Get the last 5 transactions
+                        this.currentPage =1;
+                    }
+        
+                    this.transactionDetails = transactions;
+                } else {
+                    this.showToast('Error', result.errorMessage || 'Error fetching transactions', 'error');
+                }
+            
+        } catch (error) {
+            this.showToast('Error', error.body ? error.body.message : error.message, 'error');
+        }
+
+    }
+
+    get transactionColumns() {
+        return [
+            { label: 'Transaction Type', fieldName: 'transactionType', type: 'text', cellAttributes: { alignment: 'right' }, minWidth: 150 },
+            { label: 'Amount', fieldName: 'amount', type: 'currency', cellAttributes: { alignment: 'right' }, minWidth: 150 },
+            { label: 'Units', fieldName: 'unit', type: 'currency', cellAttributes: { alignment: 'right' }, minWidth: 150 },
+            { label: 'Date of Transaction', fieldName: 'transactionDate', type: 'date', minWidth: 150 },
+            { label: 'Transaction Status', fieldName: 'transactionStatus', type: 'text', wrapText: true, minWidth: 150 }
+        ];
+    }
+
+    get totalPages() {
+        return Math.ceil(this.transactionDetails.length / this.pageSize);
+    }
+
+    get paginatedTransactions() {
+        const start = (this.currentPage - 1) * this.pageSize;
+        return this.transactionDetails.slice(start, start + this.pageSize);
+    }
+
+    handleNextPage() {
+        if (this.currentPage < this.totalPages) {
+            this.currentPage++;
+        }
+    }
+
+    handlePrevPage() {
+        if (this.currentPage > 1) {
+            this.currentPage--;
+        }
+    }
+
+    get bDisablePrevious() {
+        return this.currentPage == 1;
+    }
+    get bDisableNext() {
+        return this.currentPage == this.totalPages;
+    }
+}
