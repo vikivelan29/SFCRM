a76c158d9ef92ba0c5db7c6a833acd9e086d6b44
dhineshkumar.teekara <dhineshkumar.teekara@petronas.com>
2025-02-24 13:15:19 +0530

    ABCD VAS Dynamic Error Messages

--- force-app/main/default/lwc/abcdCreditScoreDetails/abcdCreditScoreDetails.js
+++ force-app/main/default/lwc/abcdCreditScoreDetails/abcdCreditScoreDetails.js
@@ -2,4 +2,17 @@ import { LightningElement,api } from 'lwc';
 
 export default class AbcdCreditScoreDetails extends LightningElement {
     @api csdetails;
+
+    @api status;
+    @api apiMessage;
+    get displayError(){
+        if(this.status != 'Success'){
+            return true;
+        }
+        return false;
+    }
+
+    get displayNoData() {
+        return this.status === 'Success' && (!this.csdetails || this.csdetails.length === 0 || this.csdetails == null);
+    }
 }
