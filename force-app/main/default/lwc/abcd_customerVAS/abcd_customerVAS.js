cb731849c3cca5160fac2d11c6172c1d58cfcc3b
sripavi4 <sripavithra4@gmail.com>
2025-02-25 15:31:05 +0530

    vas changes

--- force-app/main/default/lwc/abcd_customerVAS/abcd_customerVAS.js
+++ force-app/main/default/lwc/abcd_customerVAS/abcd_customerVAS.js
@@ -32,48 +32,14 @@ export default class Abcd_customerVAS extends LightningElement
                     this.apiMessage = resp.message;
                     if(resp.holdingsVASInfo.dheDetails){
                         let dheDetails = resp.holdingsVASInfo.dheDetails;
-                        if(dheDetails.lastScanDate){
-                            //let dtArr = dheDetails.lastScanDate.split('-');
-                            //dheDetails.lastScanDate = dtArr.join('/');
-                            dheDetails.lastScanDate = dheDetails.lastScanDate.includes('T') ? 
-                                                      dheDetails.lastScanDate.split('T')[0] : dheDetails.lastScanDate;
-                        } 
-                        if(dheDetails.newScanDate){
-                            dheDetails.newScanDate = dheDetails.newScanDate.includes('T')?
-                                                     dheDetails.newScanDate.split('T')[0]: dheDetails.newScanDate;
-                        }
                         this.dheDetails = dheDetails;
                     }
                     if(resp.holdingsVASInfo.csDetails){
                         let csDetails = resp.holdingsVASInfo.csDetails;
-                        if(csDetails.experianLastdownloadDate){
-                            csDetails.experianLastdownloadDate = csDetails.experianLastdownloadDate.includes('T') ?
-                                                                csDetails.experianLastdownloadDate.split('T')[0] : csDetails.experianLastdownloadDate;
-                        }
-                        if(csDetails.equifaxLastdownloadDate){
-                            csDetails.equifaxLastdownloadDate = csDetails.equifaxLastdownloadDate.includes('T') ?
-                                                                csDetails.equifaxLastdownloadDate.split('T')[0] : csDetails.equifaxLastdownloadDate;
-                        }
-                        if(csDetails.experianNewDownloadDate){
-                            csDetails.experianNewDownloadDate = csDetails.experianNewDownloadDate.includes('T') ?
-                                                                csDetails.experianNewDownloadDate.split('T')[0] : csDetails.experianNewDownloadDate;
-                        }
-                        if(csDetails.equifaxNewDownloadDate){
-                            csDetails.equifaxNewDownloadDate = csDetails.equifaxNewDownloadDate.includes('T') ?
-                                                                csDetails.equifaxNewDownloadDate.split('T')[0] : csDetails.equifaxNewDownloadDate;
-                        }
                         this.csDetails = csDetails;
                     }
                     if(resp.holdingsVASInfo.loyaltyRewards){
                         let loyaltyRewards = resp.holdingsVASInfo.loyaltyRewards;
-                        if(loyaltyRewards.lastLoyaltyCreditDate){
-                            loyaltyRewards.lastLoyaltyCreditDate = loyaltyRewards.lastLoyaltyCreditDate.includes('T') ?
-                                                            loyaltyRewards.lastLoyaltyCreditDate.split('T')[0] : loyaltyRewards.lastLoyaltyCreditDate;
-                        }
-                        if(loyaltyRewards.lastLoyaltyCreditTransaction){ 
-                            loyaltyRewards.lastLoyaltyCreditTransaction = loyaltyRewards.lastLoyaltyCreditTransaction.includes('T') ?
-                                                            loyaltyRewards.lastLoyaltyCreditTransaction.split('T')[0] : loyaltyRewards.lastLoyaltyCreditTransaction;
-                        }
                         this.loyaltyRewards = loyaltyRewards;
                     }
                 }
